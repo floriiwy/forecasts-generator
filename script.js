22f503bf24742d823655fb2407cc440d5938e0ee
floriiwy <ai3007st@gmail.com>
2024-03-05 00:33:18 +0300

    added js

--- script.js
+++ script.js
@@ -13,3 +13,32 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const prediction = document.querySelector('.current-forecast h1');
+const percent = document.querySelector('.current-forecast p');
+const list = document.querySelector('#forecast-item');
+const button = document.querySelector('button');
+const listItem = document.querySelector('.forecasts');
+
+function getRandomInt(min, max) {
+    return Math.round(Math.random() * 100);
+}
+
+function getRandomPrediction(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+let predictionNumber = getRandomPrediction(1, 4);
+let predictionText = "";
+
+if (predictionNumber == 1) {
+    predictionText = "Все будет супер!";
+} else if (predictionNumber == 2) {
+    predictionText = "Все будет еще лучше!";
+} else {
+    predictionText = "На WB отменят все комиссии";
+}
+
+const predictionItem = button.addEventListener('click', percent.append("Вероятность: " + (getRandomInt(0, 101) + '%')));
+const predictionItemText = button.addEventListener('click', prediction.prepend(predictionText));
